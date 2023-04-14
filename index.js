8c8d02992dc659a427838975d169e8d911a8577e
Shreyash Sanjay <shreyash6@hotmail.co.uk>
2023-04-14 16:02:59 +0100

    Added images and eventListener for audio

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
+
 function handleClick() {
-    alert("Test");
+    var letter = this.innerHTML;
+    
+    switch (letter) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            var bass = new Audio("sounds/kick-bass.mp3");
+            bass.play();
+            break;
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        default:
+            console.log(letter);
+            break;
+    }
 }
 
-document.querySelector("button").addEventListener("click", handleClick);
+var buttons = document.querySelectorAll(".drum");
+
+for (let index = 0; index < buttons.length; index++) {
+    buttons[index].addEventListener("click", handleClick);
+}
